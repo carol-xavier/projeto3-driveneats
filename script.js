e33bec94076dde8e0f0cd780b2cc9b99e758b749
CarolXavier <xavier.biotec@gmail.com>
2022-01-31 14:54:25 -0300

    Versao #6 - Configurações da cor e texto do botão de finalizar pedido adicionadas

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let prato = 0;
 function selecionarComida(classPrato) {
   const pratos = document.querySelector(".pratos")
   const cleanup = pratos.querySelector(".selecionado");
@@ -15,9 +16,12 @@ function selecionarComida(classPrato) {
 
   const verdinho = pratos.querySelector(classPrato);
   verdinho.classList.add("check");  
+
+
+prato = ".selecionado";
 }
 
-
+let bebida = 0;
 function selecionarBebida(classBebida) {
   const bebidas = document.querySelector(".bebidas")
   const cleanup = bebidas.querySelector(".selecionado");
@@ -35,8 +39,11 @@ function selecionarBebida(classBebida) {
 
   const verdinho = bebidas.querySelector(classBebida);
   verdinho.classList.add("check");  
+
+  bebida = classBebida;
 }
 
+let sobremesa = 0;
 function selecionarSobremesa(classSobremesa) {
   const sobremesas = document.querySelector(".sobremesas")
   const cleanup = sobremesas.querySelector(".selecionado");
@@ -54,4 +61,17 @@ function selecionarSobremesa(classSobremesa) {
 
   const verdinho = sobremesas.querySelector(classSobremesa);
   verdinho.classList.add("check");  
+
+  sobremesa = classSobremesa;
+}
+
+function ativarBotao() {
+  const botao = document.querySelector('.selecionar')
+  if (botao !== null){
+    if (prato != 0 && bebida != 0 && sobremesa !=0){
+      botao.classList.add('finalizar');
+      botao.classList.remove('selecionar');
+      botao.innerHTML = 'Fechar pedido (:'
+    }
+  }
 }
