89dfe242d72c9bd75b96b0ca31b39b5108860388
Matheus Galdino <matheus@respondeai.com.br>
2022-02-07 19:39:52 -0300

    fix dos bugs relacionados a seleção dos itens
    o código original tinha uma série de problemas relacionados com funções que não recebiam parâmetros e variáveis que recebiam os valores errados. com um pequeno refactor, esses bugs foram corrigidos e o código passou a funcionar corretamente

--- script.js
+++ script.js
@@ -1,43 +1,52 @@
-let prato = 0;
+let sectionPratos = null;
+let nomePrato = 'Prato';
 let precoPrato = 0;
-function selecionarComida(classPrato, preco) {
-  prato = document.querySelector(".pratos");
-  precoPrato = preco;
-  const cleanup = prato.querySelector(".selecionado");
+
+
+function selecionarComida(classPratoClicado, preco) {
+  sectionPratos = document.querySelector(".pratos");
+  
+  const cleanup = sectionPratos.querySelector(".selecionado");
   if (cleanup !== null) {
     cleanup.classList.remove("selecionado");
   }
-  const elemento = prato.querySelector(classPrato);
-  elemento.classList.add("selecionado");  
 
-  const check = prato.querySelector(".check");
+  const elemento = sectionPratos.querySelector(classPratoClicado);
+  precoPrato = elemento.querySelector('.preco').innerHTML;
+  nomePrato = elemento.querySelector('.nome').innerHTML;
+  elemento.classList.add("selecionado");
+
+  const check = sectionPratos.querySelector(".check");
   if (check !== null) {
     check.classList.remove("check");
   }
-  const verdinho = prato.querySelector(classPrato);
-  verdinho.classList.add("check");  
+  const verdinho = sectionPratos.querySelector(classPratoClicado);
+  verdinho.classList.add("check");
 
   ativarBotao();
 }
 
 let bebida = 0;
 let precoBebida = 0;
+
 function selecionarBebida(classBebida, preco) {
   bebida = document.querySelector(".bebidas");
-  precoBebida = preco;
+
   const cleanup = bebida.querySelector(".selecionado");
   if (cleanup !== null) {
     cleanup.classList.remove("selecionado");
   }
   const elemento = bebida.querySelector(classBebida);
-  elemento.classList.add("selecionado");  
+  precoBebida = elemento.querySelector('.preco').innerHTML;
+
+  elemento.classList.add("selecionado");
 
   const check = bebida.querySelector(".check");
   if (check !== null) {
     check.classList.remove("check");
   }
   const verdinho = bebida.querySelector(classBebida);
-  verdinho.classList.add("check");  
+  verdinho.classList.add("check");
 
   ativarBotao();
 }
@@ -46,27 +55,29 @@ let sobremesa = 0;
 let precoSobremesa = 0;
 function selecionarSobremesa(classSobremesa, preco) {
   sobremesa = document.querySelector(".sobremesas");
-  precoSobremesa = preco;
+
   const cleanup = sobremesa.querySelector(".selecionado");
   if (cleanup !== null) {
     cleanup.classList.remove("selecionado");
   }
   const elemento = sobremesa.querySelector(classSobremesa);
-  elemento.classList.add("selecionado");  
+  precoSobremesa = elemento.querySelector('.preco').innerHTML;
+
+  elemento.classList.add("selecionado");
   const check = sobremesa.querySelector(".check");
   if (check !== null) {
     check.classList.remove("check");
   }
   const verdinho = sobremesa.querySelector(classSobremesa);
-  verdinho.classList.add("check");  
+  verdinho.classList.add("check");
 
   ativarBotao();
 }
 
 function ativarBotao() {
   const botao = document.querySelector('.selecionar')
-  if (botao !== null){
-    if (prato != 0 && bebida != 0 && sobremesa !=0){
+  if (botao !== null) {
+    if (sectionPratos != 0 && bebida != 0 && sobremesa != 0) {
       botao.classList.add('finalizar');
       botao.classList.remove('selecionar');
       botao.innerHTML = 'Fechar pedido (:'
@@ -75,23 +86,32 @@ function ativarBotao() {
 }
 
 let total = 0;
-function botaoDeConfirmacao(){
-  if (prato != 0 && bebida != 0 && sobremesa != 0){
-    total = precoPrato + precoBebida + precoSobremesa;
+function botaoDeConfirmacao() {
+  if (sectionPratos != 0 && bebida != 0 && sobremesa != 0) {
+
+    let precoPratoNumber = Number(precoPrato.replace('R$ ', '').replace(',', '.'));
+    let precoBebidaNumber = Number(precoBebida.replace('R$ ', '').replace(',', '.'));
+    let precoSobremesaNumber = Number(precoSobremesa.replace('R$ ', '').replace(',', '.'));
+    
+    total = precoPratoNumber + precoBebidaNumber + precoSobremesaNumber;
     const confirmacao = document.querySelector('.confirmacao');
     confirmacao.classList.remove('escondido');
-    document.querySelector('.opcao-prato').innerHTML = prato;
-    document.querySelector('.preco-prato').innerHTML = precoPrato.toFixed(2).toString().replace('.', ',');
+
+    document.querySelector('.opcao-prato').innerHTML = nomePrato;
+    document.querySelector('.preco-prato').innerHTML = precoPrato;
+    
+    document.querySelector('.preco-bebida').innerHTML = precoBebida;
     document.querySelector('.opcao-bebida').innerHTML = bebida;
-    document.querySelector('.preco-bebida').innerHTML = precoBebida.toFixed(2).toString().replace('.', ',');
+    
     document.querySelector('.opcao-sobremesa').innerHTML = sobremesa;
-    document.querySelector('.preco-sobremesa').innerHTML = precoSobremesa.toFixed(2).toString().replace('.', ',');
-    document.querySelector('.preco-total').innerHTML = total.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+    document.querySelector('.preco-sobremesa').innerHTML = precoSobremesa;
+    
+    document.querySelector('.preco-total').innerHTML = total.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
   }
 }
 
 function cancelarPedido() {
-  const cancelar= document.querySelector('.confirmacao');
+  const cancelar = document.querySelector('.confirmacao');
   cancelar.classList.add('escondido');
 }
 
